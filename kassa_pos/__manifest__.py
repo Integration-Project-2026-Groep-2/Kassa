991c930043bd3e957e7883c1fae90eb9cd0dad95
Shems45 <shemsedine.boughaleb@student.ehb.be>
2026-05-10 23:18:15 +0200

    fix: Add BalanceButton and BalanceTopupModal to POS assets in manifest

--- kassa_pos/__manifest__.py
+++ kassa_pos/__manifest__.py
@@ -38,6 +38,8 @@
             'kassa_pos/static/src/js/ProductScreenUserButton.js',
             'kassa_pos/static/src/js/BadgeScanner.js',
             'kassa_pos/static/src/js/ClosingButton.js',
+                        'kassa_pos/static/src/js/BalanceTopupModal.js',
+                        'kassa_pos/static/src/js/BalanceButton.js',
             'kassa_pos/static/src/xml/gks_receipt.xml',
         ],
     },
